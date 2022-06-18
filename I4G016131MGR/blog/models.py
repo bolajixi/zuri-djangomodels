0d5418e3957cbdf35c6e014d5e0ba01c0c779f72
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-06-18 14:44:37 +0100

    Fix issue

--- I4G016131MGR/blog/models.py
+++ I4G016131MGR/blog/models.py
@@ -1,13 +1,16 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+
+User = get_user_model()
 
 class Post(models.Model):
     """Model definition for Post."""
 
     title = models.CharField(max_length=200)
     title = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(auto_now=True)
